fd6317b1f8be3f0f06f2e321670066cf23d6e466
Ahmed Hamdy <ahmedhamdy1515@hotmail.com>
2025-10-28 22:31:24 +0300

    Refactor ProductController to implement  comprehensive filtering and sorting logic for product retrieval ,and update sample product data in ProductsService

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,11 +1,65 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { Product } from './interfaces/product.interfaces';
+import { QueryProductDto } from './dto/query-product.dto';
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
   @Get()
-  getProduct(): Product[] {
-    return this.productService.findALlproducts();
+  getProduct(@Query() query: QueryProductDto): {
+    success: boolean;
+    message: string;
+    data: Product[];
+  } {
+    const {
+      page = 1,
+      limit = 10,
+      category,
+      minPrice,
+      maxPrice,
+      search,
+      sortBy = 'createdAt',
+      order = 'DESC',
+    } = query;
+    let fitredProducts = [...this.productService.findALlproducts()];
+    if (category) {
+      fitredProducts = fitredProducts.filter(
+        (p) => p.category.toLowerCase() === category.toLowerCase(),
+      );
+    }
+    if (minPrice !== undefined) {
+      fitredProducts = fitredProducts.filter((p) => p.price >= minPrice);
+    }
+    if (maxPrice !== undefined) {
+      fitredProducts = fitredProducts.filter((p) => p.price <= maxPrice);
+    }
+    if (search) {
+      const searchLower = search.toLowerCase();
+      fitredProducts = fitredProducts.filter(
+        (p) =>
+          p.name.toLowerCase().includes(searchLower) ||
+          p.description.toLowerCase().includes(searchLower) ||
+          p.category.toLowerCase().includes(searchLower),
+      );
+    }
+    //sorting
+    fitredProducts.sort((a, b) => {
+      let aValue = a[sortBy];
+      let bValue = b[sortBy];
+      if (sortBy === 'createdAt') {
+        aValue = new Date(aValue).getTime();
+        bValue = new Date(bValue).getTime();
+      }
+      if (order == 'ASC') {
+        return aValue > bValue ? 1 : -1;
+      } else {
+        return aValue < bValue ? 1 : -1;
+      }
+    });
+    return {
+      success: true,
+      message: 'products retrived successfuly',
+      data: fitredProducts,
+    };
   }
 }
