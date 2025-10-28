c9a7e0e20ee48b1f943daa0e30915bd1ff1d86f2
Ahmed Hamdy <ahmedhamdy1515@hotmail.com>
2025-10-28 13:54:13 +0300

    AddProduct retrival funcutionality with filtering and paginationin productsController and implement in-memory productstorage  in productservice

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ProductService } from './product.service';
-
+import { Product } from './interfaces/product.interfaces';
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+  @Get()
+  getProduct(): Product[] {
+    return this.productService.findALlproducts();
+  }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
-
+import { Product } from './interfaces/product.interfaces';
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'product1',
+      description: 'product1 description',
+      price: 100,
+      stock: 10,
+      category: 'category 1',
+      isActive: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      name: 'product2',
+      description: 'product2 description',
+      price: 200,
+      stock: 20,
+      category: 'category 2',
+      isActive: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ];
+
+  findALlproducts(): Product[] {
+    return this.products;
+  }
+}
